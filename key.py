5050bc07605effa100d45a2c6454aacf55572127
Semi_conductive_iron_box <1913720348@qq.com>
2024-08-30 23:30:40 +0800

    no message

---
+++ key.py
@@ -0,0 +1,79 @@
+import threading
+from pynput import keyboard
+import pynput
+import time
+
+my_key=["a","l","t"]#请使用非特殊键
+
+front=2 #起始工具编号
+len =3  #工具数量
+flag=False
+i=0
+j=0
+cv=threading.Condition()
+
+def on_press(key):
+    
+    global j
+    global flag
+    try:
+        
+        if(key==pynput.keyboard.KeyCode.from_char(my_key[j])):
+            j+=1
+            if(j==len(my_key)):
+                flag=True
+                j=0
+            print("true",j)
+        else:
+            j=0
+            flag=False
+        if(flag):
+            flag=False
+            global i
+            with cv:
+                i += 1
+                i = i % len       
+                cv.notify()  
+    except AttributeError:
+        pass
+def t1():
+    with keyboard.Listener(
+        on_press=on_press
+        ) as listener:
+        listener.join()
+def press_key(ctr,i):
+    for num in range(len(my_key)):
+        ctr.press(pynput.keyboard.Key.backspace)
+        ctr.release(pynput.keyboard.Key.backspace)
+    global front
+    str_i=str(i+front)  
+    print("alt+"+str_i)
+    # time.sleep(0.1)   # 增加延迟
+    ctr.press(pynput.keyboard.Key.alt_l)
+    # time.sleep(0.05)  # 增加延迟
+    ctr.press(str_i)
+    # time.sleep(0.05)  # 增加延迟
+    ctr.release(str_i)
+    # time.sleep(0.05)  # 增加延迟
+    ctr.release(pynput.keyboard.Key.alt_l)
+    # time.sleep(0.2)   # 增加延迟
+def main_loop():
+    ctr = keyboard.Controller()
+    time.sleep(1)
+    print("begin")
+    while True:
+        with cv:
+            cv.wait()  
+        press_key(ctr,i)
+
+if __name__ == '__main__':
+    thread = threading.Thread(target=t1)
+    thread.daemon = True  
+    thread.start()
+    
+    with cv:
+        cv.notify()  
+        print("b")
+    
+    main_loop()
+    
---
+++ readme.md
@@ -0,0 +1,24 @@
+环境：
+Python 3.11.4
+pynput                    1.7.7
+
+oneNote设置：
+文件-》选项
+![image](img1.png)
+自定义快速访问工具栏
+![image](img2.png)
+设置方式
+![image](img2_2.png)
+
+数位板驱动设置：
+![image](img3.png)
+这里将下键映射为顺序按下"a","l","t"
+
+代码修改：
+![image](img4.png)
+这里与上面的设置对应上即可
+未设置的为在全景手柄，橡皮擦，收藏笔2三个工具间切换
+所以长度为3，起始编号为2
+
+演示：
+![image](test.gif)
